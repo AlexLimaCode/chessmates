a8de64622c07183e45fc2b608738c9dbed03c492
alejandrolima-cbqa <alejandro.lima@cbqasolutions.com>
2022-03-19 15:17:11 -0400

    #4-install-react-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import './App.css';
 import "@material-tailwind/react/tailwind.css";
 import Button from "@material-tailwind/react/Button";
+import DefaultNavbar from "./Components/DefaultNavbar";
 
 function App() {
-    const [openNavbar, setOpenNavbar] = useState(false);
     return (
         <div className="App">
+            <div className="absolute w-full z-20">
+                <DefaultNavbar />
+            </div>
+            
             <Button color="lightBlue" ripple="light">Button</Button>
         </div>
     );
