ffd3eac8d6b12d9200e3615c0b25cb081bf20444
alejandrolima-cbqa <alejandro.lima@cbqasolutions.com>
2022-03-30 17:20:48 -0400

    version-1.1.2-fix-video-on-iphone

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -4,9 +4,17 @@ import video from '../videos/test.mp4'
 function HeroSection () {
   return (
     <div>
-      <video className="w-full object-cover -mt-3" autoplay="autoplay" muted loop="loop" webkit-playsinlin playsinline oncontextmenu="return false;" preload="auto">
+      {/* <video className="w-full object-cover -mt-3" autoplay="autoplay" muted loop="loop" webkit-playsinlin playsinline oncontextmenu="return false;" preload="auto">
         <source src={video} type="video/mp4" />
-      </video>
+      </video> */}
+      <div
+          dangerouslySetInnerHTML={{
+            __html: `<video className="w-full object-cover -mt-3" autoplay loop muted playsinline>
+                          <source src=${video} type="video/mp4" />
+                          Your browser does not support the video tag.
+                    </video>`,
+          }}
+        />
         {/* <video  src={video} autoPlay loop muted webkit-playsinlin playsinline oncontextmenu="return false;" preload="auto" type="video/mp4" /> */}
     </div>
   )
