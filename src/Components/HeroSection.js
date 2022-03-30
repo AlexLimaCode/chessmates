c91ef0e4bb15584e7a59fc785a703adcefc48143
alejandrolima-cbqa <alejandro.lima@cbqasolutions.com>
2022-03-30 17:10:26 -0400

    version-1.1.1-fix-video-on-iphone

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -4,7 +4,10 @@ import video from '../videos/test.mp4'
 function HeroSection () {
   return (
     <div>
-        <video className="w-full object-cover -mt-3" src={video} autoPlay loop muted webkit-playsinlin playsinline oncontextmenu="return false;" preload="auto" type="video/mp4" />
+      <video className="w-full object-cover -mt-3" autoplay="autoplay" muted loop="loop" webkit-playsinlin playsinline oncontextmenu="return false;" preload="auto">
+        <source src={video} type="video/mp4" />
+      </video>
+        {/* <video  src={video} autoPlay loop muted webkit-playsinlin playsinline oncontextmenu="return false;" preload="auto" type="video/mp4" /> */}
     </div>
   )
 }
